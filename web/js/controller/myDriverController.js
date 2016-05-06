46d535d417a1df07d5fad64cb939ef95c5099177
hp-godokyang <yangke@yangmatoMacBook-Pro.local>
2016-05-06 10:58:28 +0800

    机主查询司机接口绑定测试

--- web/js/controller/myDriverController.js
+++ web/js/controller/myDriverController.js
@@ -1,26 +1,40 @@
 $(function(){
 	var data = {
-		EquipmentsQuery : {
-			WeiXinOpenID : 10002
+		EquipmentDriversQuery : {
+			WeiXinOpenID : 11111111
 		}
 	};
 	data = JSON.stringify(data);
 	RetrieveMultipleEquipmentDrivers(data,success,error)
 	function success(data){
-		for (var i = 0; i < 10; i++) {
-			$('ul li:first-child div div').eq(0).text("张强"+ i);
-			$('ul li:first-child div div').eq(1).text("133333333"+ i);
-			$('ul li:first-child .driverInput').eq(0).text("2015-01-"+i);
-			$('ul li:first-child .driverInput').eq(1).text(i);
-			$("ul li:first-child").clone(true).appendTo("ul");
+		if (!data||!data.EquipmentDrivers||data.EquipmentDrivers.length <= 0) {
+			if (data&&data.ReturnValue) {
+				alert(data.ReturnValue)
+			}else{
+				alert("发生错误")
+			}
+			return
 		}
-		$("<div style='height:50px;'></div>").appendTo("ul");
-		$("ul li").each(function() {
-			$(this).click(function() {
-				alert("111")
+		for (var i = 0; i < data.EquipmentDrivers.length; i++) {
+			$('ul li:first-child div div').eq(0).text(data.EquipmentDrivers[i].DriverName);
+			$('ul li:first-child div div').eq(1).text(data.EquipmentDrivers[i].MobilePhone);
+			$('ul li:first-child .driverInput').eq(0).text(data.EquipmentDrivers[i].EmploymentStartDate);
+			$('ul li:first-child .driverInput').eq(1).text(data.EquipmentDrivers[i].ServiceHours);		
+			$("ul li:first-child").click({mydata:data.EquipmentDrivers[i]},function(event) {
+				alert(event.data.mydata.DriverId)
 				window.location.href=''
 			})
-		})
+			if (i!=data.EquipmentDrivers.length - 1) {
+				$("ul li:first-child").clone(true).appendTo("ul");
+			}	
+		}
+		$("<div style='height:50px;'></div>").appendTo("ul");
+//		$("ul li").each(function() {
+//			$(this).click(function() {
+//				alert("111")
+//				window.location.href=''
+//			})
+//		})
 		console.log("对了")
 		console.log(data)
 	};
