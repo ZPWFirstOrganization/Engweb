cabccaf921c86abaa601eff8944afcdc00affa73
hp-godokyang <yang.ke@successinfo.com.cn>
2016-05-29 02:11:28 +0800

    fix bug

--- web/js/controller/myCarController.js
+++ web/js/controller/myCarController.js
@@ -8,14 +8,14 @@ $(function(){
 			return
 		}
 		for (var i=0;i<=data.EquipmentDailyStatus.length-1;i++) {
+			for (var prop in data.EquipmentDailyStatus[i]) {
+				if (!data.EquipmentDailyStatus[i][prop]||data.EquipmentDailyStatus[i][prop] == null) {
+					data.EquipmentDailyStatus[i][prop] = ""
+				}
+			}
 			$('ul li:first-child div div').eq(0).text(data.EquipmentDailyStatus[i].BrandName+ " " +data.EquipmentDailyStatus[i].ModelName);
 			$('ul li:first-child div div').eq(1).text(data.EquipmentDailyStatus[i].DriverName);
-			if (data.EquipmentDailyStatus[i].DailyServiceHours&&data.EquipmentDailyStatus[i].DailyServiceHours!=0) {
-				$('ul li:first-child .myCarInput').eq(0).text("正常状态");//显示状态
-			} else{
-				$('ul li:first-child .myCarInput').eq(0).text("");//显示状态
-			}
-			
+			$('ul li:first-child .myCarInput').eq(0).text(data.EquipmentDailyStatus[i].EquipmentWorkStatusName);
 			$('ul li:first-child .myCarInput').eq(1).text(data.EquipmentDailyStatus[i].DailyServiceHours);
 			$('ul li:first-child .myCarInput').eq(2).text(data.EquipmentDailyStatus[i].ServiceHours);
 			$('ul li:first-child .myCarInput').eq(3).text(data.EquipmentDailyStatus[i].CurrentHours);
